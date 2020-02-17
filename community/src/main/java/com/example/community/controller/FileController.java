f5a0f4f20c7ed27181069890d75d553607372202
deng <q921598334@126.com>
2020-02-17 09:38:18 +0800

    ckeditor4的问题仍然没有解决

--- community/src/main/java/com/example/community/controller/FileController.java
+++ community/src/main/java/com/example/community/controller/FileController.java
@@ -2,22 +2,65 @@ package com.example.community.controller;
 
 
 import com.example.community.dto.FileDTO;
+import com.example.community.model.UploadImageResModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
 
 //上传图片后需要返回的结果
 @Controller
 public class FileController {
 
-    @GetMapping("/file/upload")
-    @ResponseBody
-    public FileDTO upload(){
+    private static final Logger logger = LoggerFactory.getLogger(FileController.class);
 
-        FileDTO fileDTO = new FileDTO();
-        fileDTO.setSuccess(1);
-        fileDTO.setUrl("www.baidu.com");
-        return  fileDTO;
+    @PostMapping("/uploadImage")
+    @ResponseBody
+    public UploadImageResModel uploadImage(@RequestParam("upload") MultipartFile multipartFile) {
+        UploadImageResModel res = new UploadImageResModel();
+        res.setUploaded(0);
+
+        if (multipartFile == null || multipartFile.isEmpty())
+            return res;
+
+        //生成新的文件名及存储位置
+        String fileName = multipartFile.getOriginalFilename();
+        String newFileName = UUID.randomUUID().toString()
+                .replaceAll("-", "")
+                .concat(fileName.substring(fileName.lastIndexOf(".")));
+
+
+        String fullPath = System.getProperty("user.dir")+"/src/main/resources/static/upload/".concat(newFileName);
+
+        try {
+            File target = new File(fullPath);
+            if (!target.getParentFile().exists()) { //判断文件父目录是否存在
+                target.getParentFile().mkdirs();
+            }
+
+            multipartFile.transferTo(target);
+
+
+            String imgUrl = "/upload/".concat(newFileName);
+
+            res.setUploaded(1);
+            res.setFileName(fileName);
+            res.setUrl(imgUrl);
+            return res;
+
+        } catch (IOException ex) {
+            logger.error("上传图片异常", ex);
+        }
+
+        return res;
     }
 
 }
--- community/src/main/java/com/example/community/interceptor/WebConfig.java
+++ community/src/main/java/com/example/community/interceptor/WebConfig.java
@@ -1,25 +1,25 @@
-package com.example.community.interceptor;
-
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-    @Autowired
-    LoginInterceptor loginInterceptor;
-
-    // 这个方法用来注册拦截器，我们自己写好的拦截器需要通过这里添加注册才能生效
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-
-        String[] excludes = new String[]{"/","/error/**","/question/**","/login/**","/static/**","/index/**","/css/**","/fonts/**","/images/**","/js/**","/Wopop_files/**","/callback/**"};
-        registry.addInterceptor(loginInterceptor).addPathPatterns("/**").excludePathPatterns(excludes);
-
-    }
-
-}
+//package com.example.community.interceptor;
+//
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//
+//    @Autowired
+//    LoginInterceptor loginInterceptor;
+//
+//    // 这个方法用来注册拦截器，我们自己写好的拦截器需要通过这里添加注册才能生效
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//
+//        String[] excludes = new String[]{"/","/error/**","/question/**","/login/**","/static/**","/index/**","/css/**","/fonts/**","/images/**","/js/**","/Wopop_files/**","/callback/**"};
+//        registry.addInterceptor(loginInterceptor).addPathPatterns("/**").excludePathPatterns(excludes);
+//
+//    }
+//
+//}
